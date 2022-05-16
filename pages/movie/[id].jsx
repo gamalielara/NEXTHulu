25de5b4ae9d8b51b0f40fb9d061eebfb90490f3e
gamalielara <ara.gamaliel@ui.ac.id>
2022-05-16 12:32:13 +0700

    fixed error

--- pages/movie/[id].jsx
+++ pages/movie/[id].jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../../components/Header";
-import Image from "Next/image";
+import Image from "next/image";
 import { IMAGEURL, BASEURL, MYLOADER, APIKEY } from "../../utils/variables";
 import Head from "next/head";
 import axios from "axios";
